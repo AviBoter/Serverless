0dc6efde755af470f1ceffb568e7e1919cddea9a
Avi Boter <maccavi3@gmail.com>
2021-11-25 17:55:06 +0200

    con

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_i8yMfJkaO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '48sq451a6o1068h8ancsgdb3i8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://aviserverless.auth.us-east-1.amazoncognito.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
