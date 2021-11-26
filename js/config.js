5e57fb657b0c89fc3c34b8aeb56d28d0224f9ce1
Avi Boter <maccavi3@gmail.com>
2021-11-26 14:59:32 +0200

    url

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://aviserverless.auth.us-east-1.amazoncognito.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://8q4muu2mui.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
