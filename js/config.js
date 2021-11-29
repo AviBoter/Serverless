b1651ef2eb2bfa8dd6107a0bbb9cc6389b1a43bc
Avi Boter <maccavi3@gmail.com>
2021-11-29 19:57:05 +0200

    Test

---
+++ js/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+
+// Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCwk3LpzmfZptO78BKWS4YIPW_YTHR31NI",
+  authDomain: "authwithfirebase-1.firebaseapp.com",
+  projectId: "authwithfirebase-1",
+  storageBucket: "authwithfirebase-1.appspot.com",
+  messagingSenderId: "477033394428",
+  appId: "1:477033394428:web:4b3cf282fddb3b305e2779",
+  measurementId: "G-9LSS9XLLDL"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
