3c90f1a862d70301d6c76a1016a03d01ae86baaa
Avi Boter <maccavi3@gmail.com>
2021-11-29 16:07:20 +0200

    Test

--- js/config.js
+++ js/config.js
@@ -1,17 +1,3 @@
-import { initializeApp } from 'firebase/app';
-
-var config =
- {
-    apiKey: "AIzaSyCwk3LpzmfZptO78BKWS4YIPW_YTHR31NI",
-    authDomain: "authwithfirebase-1.firebaseapp.com",
-    projectId: "authwithfirebase-1",
-    storageBucket: "authwithfirebase-1.appspot.com",
-    messagingSenderId: "477033394428",
-    appId: "1:477033394428:web:bee307574bc9c11c5e2779",
-    measurementId: "G-ZK0S7ER1B7"
- };
-
-  const app = initializeApp(firebaseConfig);
 
 /*
 window._config = {
