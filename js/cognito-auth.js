d092dde712c3524755c7e79ebb4b5bb0836023de
Avi Boter <maccavi3@gmail.com>
2021-11-30 20:48:44 +0200

    i

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -1,92 +1,9 @@
+/*global WildRydes _config AmazonCognitoIdentity AWSCognito*/
 
 var WildRydes = window.WildRydes || {};
 
 (function scopeWrapper($) {
-    
-var signinUrl = '/signin.html';
-
-// signup
-const signupForm = document.querySelector('#registrationForm');
-signupForm.addEventListener('submit', (e) => {
-e.preventDefault();
-  
-  // get user info
-  const email = $('email').val();
-  const password = $('password').val();
-
-  createUserWithEmailAndPassword(_auth, email, password)
-  .then(cred => {
-    console.log('user created:', cred.user)
-    signupForm.reset()
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-});
-
-// logout
-
-WildRydes.signOut = function signOut() {
-    //userPool.getCurrentUser().signOut();
-    window._auth.signOut().then(() => {
-    console.log('user signed out');
-  })
-};
-
-// login
-const loginForm = document.querySelector('#signinForm');
-if(loginForm)
-loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  var email = $('#emailInputSignin').val();
-  var password = $('#passwordInputSignin').val();
-
-  signInWithEmailAndPassword(_auth, email, password)
-  .then(cred => {
-    console.log('user logged in:', cred.user)
-    loginForm.reset()
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-});
-
-const unsubAuth = onAuthStateChanged(_auth, (user) => {
-    console.log('user status changed:', user)
-  });
-
-WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
-    var User = window._currentUser;
-    if (User) {
-        User.getSession(function sessionCallback(err, session) {
-            if (err) {
-                reject(err);
-            } else if (!session.isValid()) {
-                resolve(null);
-            } else {
-                resolve(session.getIdToken().getJwtToken());
-            }
-        });
-    } else {
-        resolve(null);
-    }
-});
-}(jQuery));
-
-
-
-
-
-
-
-
-/*global WildRydes _config AmazonCognitoIdentity AWSCognito
-
-
-
-
+    var signinUrl = '/signin.html';
 
     var poolData = {
         UserPoolId: _config.cognito.userPoolId,
@@ -108,13 +25,32 @@ WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject
         AWSCognito.config.region = _config.cognito.region;
     }
 
-   
+    WildRydes.signOut = function signOut() {
+        userPool.getCurrentUser().signOut();
+    };
 
-   
+    WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+        var cognitoUser = userPool.getCurrentUser();
 
-    
+        if (cognitoUser) {
+            cognitoUser.getSession(function sessionCallback(err, session) {
+                if (err) {
+                    reject(err);
+                } else if (!session.isValid()) {
+                    resolve(null);
+                } else {
+                    resolve(session.getIdToken().getJwtToken());
+                }
+            });
+        } else {
+            resolve(null);
+        }
+    });
+
+
+    /*
      * Cognito User Pool functions
-  
+     */
 
     function register(email, password, onSuccess, onFailure) {
         var dataEmail = {
@@ -168,5 +104,70 @@ WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject
         return email.replace('@', '-at-');
     }
 
-    
-    */
+    /*
+     *  Event Handlers
+     */
+
+    $(function onDocReady() {
+        $('#signinForm').submit(handleSignin);
+        $('#registrationForm').submit(handleRegister);
+        $('#verifyForm').submit(handleVerify);
+    });
+
+    function handleSignin(event) {
+        var email = $('#emailInputSignin').val();
+        var password = $('#passwordInputSignin').val();
+        event.preventDefault();
+        signin(email, password,
+            function signinSuccess() {
+                console.log('Successfully Logged In');
+                window.location.href = 'ride.html';
+            },
+            function signinError(err) {
+                alert(err);
+            }
+        );
+    }
+
+    function handleRegister(event) {
+        var email = $('#emailInputRegister').val();
+        var password = $('#passwordInputRegister').val();
+        var password2 = $('#password2InputRegister').val();
+
+        var onSuccess = function registerSuccess(result) {
+            var cognitoUser = result.user;
+            console.log('user name is ' + cognitoUser.getUsername());
+            var confirmation = ('Registration successful. Please check your email inbox or spam folder for your verification code.');
+            if (confirmation) {
+                window.location.href = 'verify.html';
+            }
+        };
+        var onFailure = function registerFailure(err) {
+            alert(err);
+        };
+        event.preventDefault();
+
+        if (password === password2) {
+            register(email, password, onSuccess, onFailure);
+        } else {
+            alert('Passwords do not match');
+        }
+    }
+
+    function handleVerify(event) {
+        var email = $('#emailInputVerify').val();
+        var code = $('#codeInputVerify').val();
+        event.preventDefault();
+        verify(email, code,
+            function verifySuccess(result) {
+                console.log('call result: ' + result);
+                console.log('Successfully verified');
+                alert('Verification successful. You will now be redirected to the login page.');
+                window.location.href = signinUrl;
+            },
+            function verifyError(err) {
+                alert(err);
+            }
+        );
+    }
+}(jQuery));
