ea94573481ca41de1dd55d04f96ba2eb8df82cb3
Avi Boter <maccavi3@gmail.com>
2021-11-30 13:36:11 +0200

    Test

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -1,6 +1,31 @@
 var WildRydes = window.WildRydes || {};
 
 (function scopeWrapper($) {
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+    // Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+      apiKey: "AIzaSyCwk3LpzmfZptO78BKWS4YIPW_YTHR31NI",
+      authDomain: "authwithfirebase-1.firebaseapp.com",
+      projectId: "authwithfirebase-1",
+      storageBucket: "authwithfirebase-1.appspot.com",
+      messagingSenderId: "477033394428",
+      appId: "1:477033394428:web:4b3cf282fddb3b305e2779",
+      measurementId: "G-9LSS9XLLDL"
+    };
+    
+    
+    // Initialize Firebase
+    window._app = initializeApp(firebaseConfig);
+    window._auth = getAuth();
+    onAuthStateChanged((user) =>{
+      if(user){
+          store.dispatch('setUser', user);
+      }else{
+          store.dispatch('setUser', null);
+      }
+    });
+    
 var signinUrl = '/signin.html';
 
 // signup
@@ -47,13 +72,7 @@ loginForm.addEventListener('submit', (e) => {
 
 });
 
-window._auth.onAuthStateChanged((user) =>{
-    if(user){
-        store.dispatch('setUser', user);
-    }else{
-        store.dispatch('setUser', null);
-    }
-  });
+
   
 WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
     var User = window._auth.currentUser;
