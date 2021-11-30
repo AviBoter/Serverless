e59a754e5b2e3b7183dd918c0869312cb0d3346e
Avi Boter <maccavi3@gmail.com>
2021-11-30 17:41:40 +0200

    V9

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -1,27 +1,3 @@
-import { initializeApp } from 'firebase/app'
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword, signOut,
-  onAuthStateChanged
-} from 'firebase/auth'
-// Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCwk3LpzmfZptO78BKWS4YIPW_YTHR31NI",
-  authDomain: "authwithfirebase-1.firebaseapp.com",
-  projectId: "authwithfirebase-1",
-  storageBucket: "authwithfirebase-1.appspot.com",
-  messagingSenderId: "477033394428",
-  appId: "1:477033394428:web:4b3cf282fddb3b305e2779",
-  measurementId: "G-9LSS9XLLDL"
-};
-
-
-// Initialize Firebase
-window._app = initializeApp(firebaseConfig);
-window._auth = getAuth();
-console.log(_auth);
 
 var WildRydes = window.WildRydes || {};
 
