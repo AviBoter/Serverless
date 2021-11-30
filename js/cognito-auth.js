c91710893241703b202fc370486b4b695cbc74a9
Avi Boter <maccavi3@gmail.com>
2021-11-30 13:21:32 +0200

    Test

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -49,7 +49,7 @@ loginForm.addEventListener('submit', (e) => {
 
 
 WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
-    var User = window._auth.User;
+    var User = window._auth.currentUser;
     if (User) {
         User.getSession(function sessionCallback(err, session) {
             if (err) {
@@ -64,75 +64,6 @@ WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject
         resolve(null);
     }
 });
-
-// listen for auth status changes
-window.func();
-    /*
-     *  Event Handlers
-     */
-   
-$(function onDocReady() {
-    $('#signinForm').submit(handleSignin);
-    $('#registrationForm').submit(handleRegister);
-    $('#verifyForm').submit(handleVerify);
-});
-
-function handleSignin(event) {
-    var email = $('#emailInputSignin').val();
-    var password = $('#passwordInputSignin').val();
-    event.preventDefault();
-    signin(email, password,
-        function signinSuccess() {
-            console.log('Successfully Logged In');
-            window.location.href = 'ride.html';
-        },
-        function signinError(err) {
-            alert(err);
-        }
-    );
-}
-
-function handleRegister(event) {
-    var email = $('#emailInputRegister').val();
-    var password = $('#passwordInputRegister').val();
-    var password2 = $('#password2InputRegister').val();
-
-    var onSuccess = function registerSuccess(result) {
-        var cognitoUser = result.user;
-        console.log('user name is ' + cognitoUser.getUsername());
-        var confirmation = ('Registration successful. Please check your email inbox or spam folder for your verification code.');
-        if (confirmation) {
-            window.location.href = 'verify.html';
-        }
-    };
-    var onFailure = function registerFailure(err) {
-        alert(err);
-    };
-    event.preventDefault();
-
-    if (password === password2) {
-        register(email, password, onSuccess, onFailure);
-    } else {
-        alert('Passwords do not match');
-    }
-}
-
-function handleVerify(event) {
-    var email = $('#emailInputVerify').val();
-    var code = $('#codeInputVerify').val();
-    event.preventDefault();
-    verify(email, code,
-        function verifySuccess(result) {
-            console.log('call result: ' + result);
-            console.log('Successfully verified');
-            alert('Verification successful. You will now be redirected to the login page.');
-            window.location.href = signinUrl;
-        },
-        function verifyError(err) {
-            alert(err);
-        }
-    );
-}
 }(jQuery));
 
 
